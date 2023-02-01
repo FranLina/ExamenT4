81b44a60c3cf4571c0981f40971d55076a036e0d
francisco Linares <francisco.linares.536@estudiante.iesoretania.es>
2023-02-01 08:39:35 +0100

    Commit Inicial

---
+++ src/test/java/com/flb/exament4/Exament4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flb.exament4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exament4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
